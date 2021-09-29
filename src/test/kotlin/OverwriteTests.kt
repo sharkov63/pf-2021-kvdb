5c2077888845d5add095ffd10f1ed773cc2583aa
Данила <duesna897@gmail.com>
2021-09-29 22:11:17 +0300

    Added overwrite tests and "no parent directories" test (for create and overwrite)

--- src/test/kotlin/OverwriteTests.kt
+++ src/test/kotlin/OverwriteTests.kt
@@ -2,14 +2,14 @@ import dbfile.*
 import java.io.File
 import kotlin.test.*
 
-internal class CreateTests {
-    private val testDataDir = "testData/create"
+internal class OverwriteTests {
+    private val testDataDir = "testData/overwrite"
 
     // A test where no exitProcess() is called
-    private fun correctCreateTestTemplate(testName: String, data: Map<String, String>) {
+    private fun correctOverwriteTestTemplate(testName: String, data: Map<String, String>) {
         val dbPath = "$testDataDir/$testName.db"
         val correctDBPath = "$testDataDir/$testName-correct.db"
-        createDataBase(dbPath, data)
+        overwriteDataBase(dbPath, data)
         val dbFile = File(dbPath)
         val correctDBFile = File(correctDBPath)
         assertEquals(correctDBFile.readText(), dbFile.readText())
@@ -17,21 +17,21 @@ internal class CreateTests {
     }
 
     // A test where exitProcess() should be called
-    private fun incorrectCreateTestTemplate(testName: String, data: Map<String, String>) {
+    private fun incorrectOverwriteTestTemplate(testName: String, data: Map<String, String>) {
         val noExitSecurityManager = NoExitSecurityManager()
         System.setSecurityManager(noExitSecurityManager)
-        assertFails { correctCreateTestTemplate(testName, data) }
+        assertFails { correctOverwriteTestTemplate(testName, data) }
     }
 
     @Test
-    fun createTest() {
-        val testName = "createTest"
+    fun overwriteTest() {
+        val testName = "overwriteTest"
         val data = mapOf(
             "first" to "second",
             "a" to "b",
             "xxxx" to "$",
         )
-        correctCreateTestTemplate(testName, data)
+        correctOverwriteTestTemplate(testName, data)
     }
 
     @Test
@@ -40,14 +40,14 @@ internal class CreateTests {
         val data = mapOf(
             "" to "",
         )
-        correctCreateTestTemplate(testName, data)
+        correctOverwriteTestTemplate(testName, data)
     }
 
     @Test
     fun testEmptyData() {
         val testName = "testEmptyData"
         val data: Map<String, String> = mapOf()
-        correctCreateTestTemplate(testName, data)
+        correctOverwriteTestTemplate(testName, data)
     }
 
     @Test
@@ -66,7 +66,7 @@ internal class CreateTests {
         )
         val dbFile = File("$testDataDir/$testName.db")
         writeDatabaseToFile(dbFile, dataInAlreadyCreated)
-        incorrectCreateTestTemplate(testName, data)
+        correctOverwriteTestTemplate(testName, data)
     }
 
     @Test
@@ -80,11 +80,13 @@ internal class CreateTests {
         val dbFilePath = "$testDataDir/$testSubPath"
         val correctDBFilePath = "$testDataDir/$testName-correct.db"
         val correctDBFile = File(correctDBFilePath)
-        createDataBase(dbFilePath, data)
+        overwriteDataBase(dbFilePath, data)
         val dbFile = File(dbFilePath)
         assertEquals(correctDBFile.readText(), dbFile.readText())
         assert(dbFile.delete())
         val testDir = File("$testDataDir/$testName")
         assert(testDir.deleteRecursively())
     }
+
+
 }
