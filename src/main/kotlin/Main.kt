8f4d39a8c5a0f151200da0b71e29d4462dc031cd
Данила <duesna897@gmail.com>
2021-09-29 18:21:34 +0300

    Added option parsing and basic exit messages

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,12 +1,28 @@
 import io.*
+import java.io.File
+import kotlin.system.exitProcess
 
-fun printHelp() {
-    TODO()
+fun printIncorrectArgsMsg() {
+    println("Incorrect arguments. Please use -h or --help option to see help message.")
+    exitProcess(1)
+}
+
+fun printHelpMsg() {
+    TODO("write help message")
+    exitProcess(0)
+}
+
+fun parseOption(option: String, args: List<String>) {
+    when (option) {
+        "-h", "--help" -> printHelpMsg()
+        else -> printIncorrectArgsMsg()
+    }
 }
 
 fun main(args: Array<String>) {
-    if (args.isEmpty()) return printHelp()
+    if (args.isEmpty()) return printIncorrectArgsMsg()
 
-
-
+    val option = args.first()
+    args.drop(1)
+    parseOption(option, args.toList())
 }
