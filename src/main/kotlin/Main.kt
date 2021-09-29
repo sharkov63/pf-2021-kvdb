b4ca012f3b939fa1fe7997993bbdc60ed33d9abc
Данила <duesna897@gmail.com>
2021-09-29 18:22:38 +0300

    Added -r (--read) option

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -12,8 +12,45 @@ fun printHelpMsg() {
     exitProcess(0)
 }
 
+fun printDatabaseNotExistMsg(dbPath: String) {
+    println("Database at \"$dbPath\" does not exist!")
+    exitProcess(1)
+}
+
+fun printInvalidDatabaseMsg(dbPath: String) {
+    println("Database at \"$dbPath\" is not valid!")
+    exitProcess(1)
+}
+
+fun printDatabaseNotContainsKey(dbPath: String, key: String) {
+    println("Database at \"$dbPath\" doesn't contain the key \"$key\".")
+    exitProcess(1)
+}
+
+fun readKeys(dbFileName: String, keys: List<String>) {
+    val dbFile = File(dbFileName)
+    if (!dbFile.exists()) {
+        return printDatabaseNotExistMsg(dbFile.path)
+    }
+    val data = readDatabaseFromFile(dbFile) ?: return printInvalidDatabaseMsg(dbFile.path)
+    keys.forEach { key ->
+        if (!data.containsKey(key)) {
+            return printDatabaseNotContainsKey(dbFile.path, key)
+        }
+        println(data[key])
+    }
+}
+
+fun parseReadArgs(args: List<String>) {
+    if (args.size < 2) return printIncorrectArgsMsg()
+    val dbFileName = args[0]
+    args.drop(1)
+    readKeys(dbFileName, args.toList())
+}
+
 fun parseOption(option: String, args: List<String>) {
     when (option) {
+        "-r", "--read" -> parseReadArgs(args)
         "-h", "--help" -> printHelpMsg()
         else -> printIncorrectArgsMsg()
     }
