827b0cb701174863e9c5750ce605ce84ff6d327b
Данила <duesna897@gmail.com>
2021-09-30 17:28:10 +0300

    Documented ExitMsg.kt

--- src/main/kotlin/exitMsg/ExitMsg.kt
+++ src/main/kotlin/exitMsg/ExitMsg.kt
@@ -2,6 +2,15 @@ package exitMsg
 
 import kotlin.system.exitProcess
 
+/**
+ * This package contains exit functions, which print a certain message,
+ * and then call [exitProcess] with a corresponding exit status.
+ *
+ * Exit status 0 means the program finished with no errors.
+ * Exit status 1 means the program finished with an error.
+ */
+
+
 fun printIncorrectArgsMsg() {
     println("Incorrect arguments. Please use -h or --help option to see help message.")
     exitProcess(1)
