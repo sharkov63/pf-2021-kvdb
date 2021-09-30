03b138a151e84e4548ce13f021b49a59caa7e125
Данила <duesna897@gmail.com>
2021-09-30 18:00:17 +0300

    Documented and corrected Operations.kt

--- src/main/kotlin/operations/Operations.kt
+++ src/main/kotlin/operations/Operations.kt
@@ -1,114 +1,171 @@
 package operations
 
 import dbfile.*
-import exitMsg.*
+import exitFuncs.*
 
 import java.io.File
 import java.nio.file.Files.createDirectories
 import java.nio.file.Path
 
+/**
+ * This package contains possible operations with databases.
+ */
 
 
+/* Aux functions */
+
+private fun ensureAncestorDirectories(file: File) {
+    val parentPath = Path.of(file.parent)
+    createDirectories(parentPath)
+}
+
+private fun ensureFile(file: File) {
+    ensureAncestorDirectories(file)
+    file.createNewFile()
+}
+
+
+/**
+ * Prints the values of [keys] in a database at [dbFileName]
+ * to stdout, each value on a separate line.
+ *
+ * If the database does not exist, cannot be read, or does not match the format,
+ * it calls a corresponding exit function.
+ *
+ * On the first key, which is not present in the database,
+ * calls [exitDatabaseNotContainsKey] exit function.
+ */
 fun readKeys(dbFileName: String, keys: List<String>) {
     val dbFile = File(dbFileName)
     if (!dbFile.exists()) {
-        return printDatabaseNotExistMsg(dbFile.path)
+        return exitDatabaseNotExist(dbFile.path)
     }
     if (!dbFile.canRead()) {
-        return printCannotReadDataBase(dbFile.path)
+        return exitCannotReadDataBase(dbFile.path)
     }
-    val data = readDatabaseFromFile(dbFile) ?: return printInvalidDatabaseMsg(dbFile.path)
+    val data = readDatabaseFromFile(dbFile) ?: return exitInvalidDatabase(dbFile.path)
     keys.forEach { key ->
         if (!data.containsKey(key)) {
-            return printDatabaseNotContainsKey(dbFile.path, key)
+            return exitDatabaseNotContainsKey(dbFile.path, key)
         }
         println(data[key])
     }
 }
 
+
+/**
+ * Creates a new database at [dbFileName] (with all the directories) and writes [data] in it.
+ *
+ * If the file already exists, or it cannot write to a newly created file,
+ * it calls a corresponding exit function.
+ */
 fun createDataBase(dbFileName: String, data: Map<String, String>) {
     val dbFile = File(dbFileName)
-    val dbParentPath = Path.of(dbFile.parent)
-    createDirectories(dbParentPath)
+    ensureAncestorDirectories(dbFile)
     if (!dbFile.createNewFile()) {
-        return printDataBaseAlreadyExists(dbFile.path)
+        return exitDataBaseAlreadyExists(dbFile.path)
     }
     if (!dbFile.canWrite()) {
-        return printCannotWriteToDataBase(dbFile.path)
+        return exitCannotWriteToDataBase(dbFile.path)
     }
     writeDatabaseToFile(dbFile, data)
 }
 
+/**
+ * Writes [data] to the database at [dbFileName].
+ * Overwrites if the database already exists.
+ * Creates the database (and all required directories) otherwise.
+ *
+ * If it cannot write to the file, calls an exit function.
+ */
 fun overwriteDataBase(dbFileName: String, data: Map<String, String>) {
     val dbFile = File(dbFileName)
-    val dbParentPath = Path.of(dbFile.parent)
-    createDirectories(dbParentPath)
-    dbFile.createNewFile()
+    ensureFile(dbFile)
     if (!dbFile.canWrite()) {
-        return printCannotWriteToDataBase(dbFile.path)
+        return exitCannotWriteToDataBase(dbFile.path)
     }
     writeDatabaseToFile(dbFile, data)
 }
 
+
+/**
+ * Adds [dataToAdd] to the database at [originalDBFileName]
+ * and saves the result to the database at [newDBFileName] (which may be equal to [originalDBFileName])
+ * Records, in which keys are already present in the original database, are omitted.
+ * Writes the number of written and omitted records to stdout.
+ *
+ * If the original database cannot be reached or read, or is invalid,
+ * or it cannot write to the new database, calls a corresponding exit function.
+ *
+ * If there is no data to add, calls [exitNoDataToWrite].
+ */
 fun addToDataBase(originalDBFileName: String, newDBFileName: String, dataToAdd: Map<String, String>) {
     if (dataToAdd.isEmpty())
-        return printNoDataToWriteMsg()
+        return exitNoDataToWrite()
 
     val originalDBFile = File(originalDBFileName)
     val newDBFile = File(newDBFileName)
 
     if (originalDBFileName != newDBFileName && !originalDBFile.exists())
-        return printDatabaseNotExistMsg(originalDBFile.path)
+        return exitDatabaseNotExist(originalDBFile.path)
 
-    val newDBParentPath = Path.of(newDBFile.parent)
-    createDirectories(newDBParentPath)
-    newDBFile.createNewFile()
+    if (!originalDBFile.canRead())
+        return exitCannotReadDataBase(originalDBFile.path)
 
-    if (!originalDBFile.canRead()) {
-        return printCannotReadDataBase(originalDBFile.path)
-    }
-
-    val data = readDatabaseFromFile(originalDBFile) ?: return printInvalidDatabaseMsg(originalDBFile.path)
+    val data = readDatabaseFromFile(originalDBFile) ?: return exitInvalidDatabase(originalDBFile.path)
     val dataToWrite = data + dataToAdd.filterKeys { key -> !data.containsKey(key) }
     val omittedRecords = dataToAdd.count { (key, _) -> data.containsKey(key) }
     val writtenRecords = dataToAdd.size - omittedRecords
+
+    ensureFile(newDBFile)
     if (!newDBFile.canWrite()) {
-        return printCannotWriteToDataBase(newDBFile.path)
+        return exitCannotWriteToDataBase(newDBFile.path)
     }
     writeDatabaseToFile(newDBFile, dataToWrite)
+
     if (writtenRecords > 0)
         println("Successfully written $writtenRecords records to database at \"${newDBFile.path}\".")
     if (omittedRecords > 0)
         println("$omittedRecords records were omitted, as database already contains their keys.")
 }
 
+
+/**
+ * Writes [dataToWrite] to the database at [originalDBFileName]
+ * and saves the result to the database at [newDBFileName] (which may be equal to [originalDBFileName])
+ * Records, in which keys are already present in the original database, overwrite the database.
+ * Writes the number of written and overwriting records to stdout.
+ *
+ * If the original database cannot be reached or read, or is invalid,
+ * or it cannot write to the new database, calls a corresponding exit function.
+ *
+ * If there is no data to add, calls [exitNoDataToWrite].
+ */
 fun writeToDataBase(originalDBFileName: String, newDBFileName: String, dataToWrite: Map<String, String>) {
     if (dataToWrite.isEmpty())
-        return printNoDataToWriteMsg()
+        return exitNoDataToWrite()
 
     val originalDBFile = File(originalDBFileName)
     val newDBFile = File(newDBFileName)
 
     if (originalDBFileName != newDBFileName && !originalDBFile.exists())
-        return printDatabaseNotExistMsg(originalDBFile.path)
+        return exitDatabaseNotExist(originalDBFile.path)
 
-    val newDBParentPath = Path.of(newDBFile.parent)
-    createDirectories(newDBParentPath)
-    newDBFile.createNewFile()
+    if (!originalDBFile.canRead())
+        return exitCannotReadDataBase(originalDBFile.path)
 
-    if (!originalDBFile.canRead()) {
-        return printCannotReadDataBase(originalDBFile.path)
-    }
-
-    val data = readDatabaseFromFile(originalDBFile) ?: return printInvalidDatabaseMsg(originalDBFile.path)
+    val data = readDatabaseFromFile(originalDBFile) ?: return exitInvalidDatabase(originalDBFile.path)
     val overwrittenRecords = dataToWrite.count { (key, _) -> data.containsKey(key) }
     val writtenRecords = dataToWrite.size
+
+    ensureFile(newDBFile)
     if (!newDBFile.canWrite()) {
-        return printCannotWriteToDataBase(newDBFile.path)
+        return exitCannotWriteToDataBase(newDBFile.path)
     }
     writeDatabaseToFile(newDBFile, data + dataToWrite)
+
     if (writtenRecords > 0)
         println("Successfully written $writtenRecords records to database at \"${newDBFile.path}\".")
     if (overwrittenRecords > 0)
-        println("$overwrittenRecords of those records were overwritten")
+        println("$overwrittenRecords of those records overwrote the data.")
 }
